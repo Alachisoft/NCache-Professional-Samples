0cb72186fb192b54ee2992a81b8b700c6876a9e6
DIYATECH\muhammad_danyial <muhammad_danyial@diyatech.com>
2021-03-31 16:21:09 +0500

    Samples for NCache Professional DotNet Framework is updated. Files changed: App.config, packages.config, web.config, appsettings.json, AssemblyInfo.cs, ReadMe.md Updated Cache names to demoCache, Updated nuget version, copyrights.

---
+++ dotnet/DistributedLucene/DistributedLucene/README.md
@@ -0,0 +1,65 @@
+# DISTRIBUTED LUCENE
+
+### Table of contents
+
+* [Introduction](#introduction)
+* [Prerequisites]
+	- Lucene Indexing should be enabled.
+* [Build and Run the sample](#build-and-run-the-sample)
+* [Additional Resources](#additional-resources)
+* [Technical Support](#technical-support)
+* [Copyrights](#copyrights)
+
+### Introduction
+
+This sample program demonstrates how to use the NCache Distributed Lucene API. 
+It shows how to initialize the cache instance and perform Lucene related tasks from NCache.
+
+### Prerequisites
+
+Before the sample application is executed make sure that:
+
+- A Partitioned cache is created.
+- app.config have been changed according to the configurations. 
+	- Change the cache name as required.
+	- Change the index name as required.
+- By default this sample uses 'demoCache' and 'MyIndex', make sure that cache is running.
+
+- To use this sample, you must first enable Lucene Indexes on cache. 
+	- i.e  
+		<cache-config cache-name="demoCache">
+		  <cache-settings>
+			<client-activity-notification enabled="True" retention-period="5sec"/>
+			  <caching-modules>
+				<module name="lucene-net-4.8" enable="True">
+				   <parameters name="IndexPath" value="C:\Program Files\NCache\bin\modules\data\lucene"/>
+				</module>
+			  </caching-modules>
+			</cache-settings>
+		 </cache-config>
+	 
+
+### Build and Run the Sample
+    
+- Run the sample application.
+
+### Additional Resources
+
+##### Documentation
+The complete online documentation for NCache is available at:
+http://www.alachisoft.com/resources/docs/#ncache
+
+##### Programmers' Guide
+The complete programmers guide of NCache is available at:
+http://www.alachisoft.com/resources/docs/ncache/prog-guide/
+
+### Technical Support
+
+Alachisoft [C] provides various sources of technical support. 
+
+- Please refer to http://www.alachisoft.com/support.html to select a support resource you find suitable for your issue.
+- To request additional features in the future, or if you notice any discrepancy regarding this document, please drop an email to [support@alachisoft.com](mailto:support@alachisoft.com).
+
+### Copyrights
+
+[C] Copyright 2021 Alachisoft 
---
+++ dotnet/DistributedPubSubSample/Subscriber/Program.cs
@@ -0,0 +1,61 @@
+﻿// ===============================================================================
+// Alachisoft (R) NCache Sample Code.
+// ===============================================================================
+// Copyright © Alachisoft.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+// ===============================================================================
+
+using Alachisoft.NCache.Runtime.Caching;
+using System;
+using System.Configuration;
+
+namespace Alachisoft.NCache.Samples
+{
+    /// ******************************************************************************
+    /// <summary>
+    /// A sample program that demonstrates how to use the messaging api in NCache.
+    /// 
+    /// Requirements:
+    ///     1. A running NCache cache
+    ///     2. Connection attributes in app.config
+    /// </summary>
+    /// 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string cacheId = ConfigurationManager.AppSettings["CacheId"];
+                string topicName = "mytopic";
+
+                // Creates a subscriber's instance.
+                using (Subscriber subscriber = new Subscriber())
+                {
+                    // Subscribes on it, using the provided cache-Id and the topic-name.
+                    subscriber.Subscribe(cacheId, topicName, MessageReceivedCallback);                    
+
+                    Console.WriteLine("Subscriber Started. Press enter to exit.");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// This method will get invoked if a message is recieved by the subscriber.
+        /// </summary>
+        static void MessageReceivedCallback(object sender, MessageEventArgs args)
+        {
+            Console.WriteLine("Message Recieved: {0}", args.Message.Payload);
+        }
+
+        
+    }
+}
